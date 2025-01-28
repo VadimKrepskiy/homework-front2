456ccf1e1bd1996cd5c018cf3e3fcbb0044e51ce
Vadim Krepskiy <vadim.krepski@gmail.com>
2025-01-28 14:48:12 +0300

    Fix task 4

--- src/s2-homeworks/hw04/HW4.tsx
+++ src/s2-homeworks/hw04/HW4.tsx
@@ -12,10 +12,12 @@ import Stand from './Stand'
 const HW4 = () => {
     return (
         <div id={'hw4'}>
-            <div className={s2.hwTitle}>Homework #4</div>
+            <div className={s2.hwTitle + ' ' + s2.container}>Homework #4</div>
             {/*демонстрация возможностей компонент:*/}
             <div className={s2.hw}>
-                <Stand />
+                <div className={s2.container}>
+                    <Stand />
+                </div>
             </div>
         </div>
     )
