c5e7620cfa42a34441215481f89e97ccd3988fea
Vadim Krepskiy <vadim.krepski@gmail.com>
2025-01-28 15:17:40 +0300

    Fix onChangeCallback function

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,9 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeChecked?.(e.currentTarget.checked)// задачка на написание онченджа
+        if(onChangeChecked)
+            onChangeChecked(e.currentTarget.checked)
+        else onChange?.(e)// задачка на написание онченджа
     }
 
     const finalInputClassName = s.checkbox
