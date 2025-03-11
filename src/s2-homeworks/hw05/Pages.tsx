dacdd9283e122594d5a9b6276cc139f4a32c8c06
VadimKrepskiy <krepskiy.vadim@mail.ru>
2025-03-11 14:49:09 +0300

    Fix task 7

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -17,7 +17,7 @@ function Pages() {
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                <Route path='/' element={<Junior />} />
+                <Route path='/' element={<PreJunior />} />
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
