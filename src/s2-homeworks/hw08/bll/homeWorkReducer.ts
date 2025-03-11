eda2ccf2ed91766d8818147daee7e6fa978a6259
VadimKrepskiy <vadim.krepski@gmail.com>
2025-03-11 20:52:16 +0300

    Fix task 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            let compare = (a: UserType, b: UserType):number => a.name < b.name ? -1 : 1;
+            if(action.payload === 'down')
+                compare = (a: UserType, b: UserType):number => a.name > b.name ? -1 : 1;
+            state.sort((a,b) => compare(a,b))
             return state // need to fix
         }
         case 'check': {
-
+            state = state.filter((a:UserType) => a.age >= 18)
             return state // need to fix
         }
         default:
