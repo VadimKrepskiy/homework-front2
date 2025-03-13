8d05d258829f5481584eadbc619d1d348a207906
VadimKrepskiy <vadim.krepski@gmail.com>
2025-03-13 20:43:49 +0300

    Fix setLoading function

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -22,7 +22,7 @@ const HW10 = () => {
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
         dispatch(loadingAC(!isLoading))
-        setTimeout(() => {},1500)
+        setTimeout(() => dispatch(loadingAC(isLoading)),1500)
         // setTimeout
     }
 
